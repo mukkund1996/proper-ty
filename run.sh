d0493492e60c43a79d46092a59d43ccab614ff56
Mukkund Sunjii <m.babugheethaa@shell.com>
2024-05-13 19:20:45 +0200

    chore: Add production environment variables and update docker-compose file

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+touch client/.env.production
+echo VITE_API_URL="http://localhost:8000/api" >> client/.env.production
+echo VITE_MAPLIBRE_TOKEN="YOURTOKEN" >> client/.env.production
+
 docker-compose up -d
